a4e17321c0363841642bb62d1771317a2350307c
Vedank Naik <vedankn01@gmail.com>
2022-10-09 14:16:56 +0530

    Constants + resources

---
+++ src/utils/constants.js
@@ -0,0 +1,45 @@
+import MusicNoteIcon from "@mui/icons-material/MusicNote";
+import HomeIcon from "@mui/icons-material/Home";
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import CodeIcon from "@mui/icons-material/Code";
+import OndemandVideoIcon from "@mui/icons-material/OndemandVideo";
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import LiveTvIcon from "@mui/icons-material/LiveTv";
+import SchoolIcon from "@mui/icons-material/School";
+import FaceRetouchingNaturalIcon from "@mui/icons-material/FaceRetouchingNatural";
+import CheckroomIcon from "@mui/icons-material/Checkroom";
+import GraphicEqIcon from "@mui/icons-material/GraphicEq";
+import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
+import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
+import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+
+import noImage from "../assets/no-image.jpg";
+import logopng from "../assets/logo.png";
+
+export const logo = logopng;
+
+export const categories = [
+  { name: "New", icon: <HomeIcon /> },
+  { name: "Trending", icon: <TrendingUpIcon /> },
+  { name: "Live", icon: <LiveTvIcon /> },
+  { name: "Coding", icon: <CodeIcon /> },
+  { name: "Music", icon: <MusicNoteIcon /> },
+  { name: "Education", icon: <SchoolIcon /> },
+  { name: "Podcast", icon: <GraphicEqIcon /> },
+  { name: "Movie", icon: <OndemandVideoIcon /> },
+  { name: "Gaming", icon: <SportsEsportsIcon /> },
+  { name: "Sport", icon: <FitnessCenterIcon /> },
+  { name: "Fashion", icon: <CheckroomIcon /> },
+  { name: "Beauty", icon: <FaceRetouchingNaturalIcon /> },
+  { name: "Comedy", icon: <TheaterComedyIcon /> },
+  { name: "Gym", icon: <FitnessCenterIcon /> },
+  { name: "Crypto", icon: <DeveloperModeIcon /> },
+];
+
+export const demoThumbnailUrl = noImage;
+export const demoChannelUrl = "/channel/UCgUDkHyf9NXeisgZWCLbsZA";
+export const demoVideoUrl = "/video/K4TOrB7at0Y";
+export const demoChannelTitle = "frostbyte";
+export const demoVideoTitle = "Demo Background Sample Video";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
